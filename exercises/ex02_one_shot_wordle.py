a58f9f78d2b85cdc32ecca9ee0ef505138c06f22
Sydney Schaefer <sydney1s@unc.edu>
2022-09-05 23:30:10 -0400

    new changes

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,4 +1,4 @@
-"""EX02 - One shot Wordle"""
+"""EX02 - One shot Wordle."""
 
 __author__ = "730557198"
 
@@ -7,8 +7,8 @@ word: str = "python"
 wrong_len: int = randint(1, 2)
 
 guess: str = str(input("What is your 6-letter guess? "))
-while(len(guess) != 6):
-    if(wrong_len == 1):
+while len(guess) != 6:
+    if wrong_len == 1:
         guess = str(input("That was not 6 letters! Try again: "))
     else:
         print("Not quite! Play again soon!")
@@ -21,19 +21,24 @@ YELLOW_BOX: str = "\U0001F7E8"
 i: int = 0
 emoji: str = ""
 yellow: bool = False
-while(i < len(word)):
-    if(word[i] == guess[i]):
+while i < len(word):
+    if word[i] == guess[i]:
         emoji += GREEN_BOX
     else:
         j: int = 0
-        while(yellow == False and j < len(word)):
-            if(word[j] == guess[i]):
+        while yellow == False and j < len(word):
+            if word[j] == guess[i]:
                 yellow = True
             j += 1
-        if(yellow == True):
+        if yellow:
             emoji += YELLOW_BOX
         else:
             emoji += WHITE_BOX
     i += 1
 
 print(emoji)
+
+if guess == word:
+    print("Woo! You got it!")
+else:
+    print("Not quite. Play again soon!")
